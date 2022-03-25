bf791573e445260b59bd3732ec8adf34fdfd0df8
kalaljagruti24 <102379799+kalaljagruti24@users.noreply.github.com>
2022-03-25 23:34:57 +0530

    Add files via upload

---
+++ oprators.cs
@@ -0,0 +1,26 @@
+﻿/* Name:- Kalal Jagruti Machhindra
+   Class :- SYBCA(32) */
+using System; 
+namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = 18, secondNumber = 5, result;
+            int num1 = 20, num2 = 6, rem;
+            result = firstNumber + secondNumber;
+            Console.WriteLine(" Addition = {0} + {1} = {2}", firstNumber, secondNumber, result);
+
+            result = firstNumber * secondNumber;
+            Console.WriteLine(" Multiplication = {0} * {1} = {2}", firstNumber, secondNumber, result);
+
+            result = firstNumber / secondNumber;
+            Console.WriteLine(" Division = {0} / {1} = {2}", firstNumber, secondNumber, result);
+
+            result = firstNumber - secondNumber;
+            Console.WriteLine(" Subraction = {0} - {1} = {2}", firstNumber, secondNumber, result);
+
+        }
+    }
+}
