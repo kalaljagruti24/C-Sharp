164acca1c8d6ca7e29f18cd5e2bba3d46348712b
kalaljagruti24 <102379799+kalaljagruti24@users.noreply.github.com>
2022-03-25 23:08:04 +0530

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int jagu = 24;
+           // string inp = Console.ReadLine();
+           // Console.WriteLine(inp);
+            Console.WriteLine("Hello World");
+            Console.Write("Hello jagu");
+            Console.WriteLine(" i love c#"+jagu);
+            Console.ReadLine();
+        }
+    }
+}
