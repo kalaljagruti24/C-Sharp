71ff5693aeb1294a9920ff503cd1d50ad2834939
kalaljagruti24 <102379799+kalaljagruti24@users.noreply.github.com>
2022-03-25 23:12:24 +0530

    Add files via upload

---
+++ simpl.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to");
+
+            
+            
+        }
+    }
+}
