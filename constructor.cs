c3b83c813368991e1ca8beb5cbddc8e0e6a45122
kalaljagruti24 <102379799+kalaljagruti24@users.noreply.github.com>
2022-03-25 23:24:45 +0530

    Add files via upload

---
+++ constructor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    internal class Program
+    { 
+            static void Main(string[] args)
+            {
+                int i;
+                Console.WriteLine("Enter the no to print the table:- \n\n");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("--------For Loop Execution--------\n\n");
+                for (i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine( i*n+"\n");
+
+                }
+                Console.WriteLine("\n");
+            Console.ReadLine();
+            }
+        }
+    }
+
