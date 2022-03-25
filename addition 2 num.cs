fca7a6df2d86c609b88b1e9367612484070d419c
kalaljagruti24 <102379799+kalaljagruti24@users.noreply.github.com>
2022-03-25 23:11:01 +0530

    Add files via upload

---
+++ addition 2 num.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, result;
+            Console.WriteLine("Enter  number 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter  number 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter  number 3");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            result = num1 + num2 + num3;
+
+            Console.WriteLine("{0} + {1} + {2} ={3}",num1,num2, num3 ,result);
+
+             
+                
+        }
+    }
+}
+
